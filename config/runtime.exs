aaa846198019e9b4d54361c1a0b61a5a64d31f0e
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2022-03-07 00:04:29 +0700

    update config/runtime.exs for the prefix worker.

--- config/runtime.exs
+++ config/runtime.exs
@@ -9,13 +9,13 @@ config :alephium_miner_bot, :alph_node_port, System.fetch_env!("ALPH_NODE_PORT")
 config :alephium_miner_bot, :alph_node_api_key, System.fetch_env!("ALPH_NODE_API_KEY")
 config :alephium_miner_bot, :alph_node_wallet_name, System.fetch_env!("ALPH_NODE_WALLET_NAME")
 config :alephium_miner_bot, :alph_node_wallet_password, System.fetch_env!("ALPH_NODE_WALLET_PASSWORD")
-config :alephium_miner_bot, :alph_hashrate_interval, System.fetch_env!("ALPH_WORKER_HASHRATE_INTERVAL") |> String.to_integer()
+config :alephium_miner_bot, :alph_worker_hashrate_interval, System.fetch_env!("ALPH_WORKER_HASHRATE_INTERVAL") |> String.to_integer()
 
 # Kaspa Configuration
 config :alephium_miner_bot, :kaspa_worker, if(System.fetch_env!("KASPA_WORKER") == "true", do: true, else: false)
 config :alephium_miner_bot, :kaspa_wallet_path, System.fetch_env!("KASPA_WALLET_PATH")
 config :alephium_miner_bot, :kaspa_ctl_path, System.fetch_env!("KASPA_CTL_PATH")
-config :alephium_miner_bot, :kaspa_hashrate_interval, System.fetch_env!("KASPA_WORKER_HASHRATE_INTERVAL") |> String.to_integer()
+config :alephium_miner_bot, :kaspa_worker_hashrate_interval, System.fetch_env!("KASPA_WORKER_HASHRATE_INTERVAL") |> String.to_integer()
 
 # IP Worker Configuration
 config :alephium_miner_bot, :worker_ip_interval, System.fetch_env!("WORKER_IP_INTERVAL") |> String.to_integer()
